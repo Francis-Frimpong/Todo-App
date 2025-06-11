f4081c60a7a9f61e144ac9b254ab150280694728
Francis <brakay1136@gmail.com>
2025-06-11 13:14:26 -0700

    created an html element using javascript

--- app.js
+++ app.js
@@ -3,21 +3,39 @@ const listContainer = document.querySelector(".list-container");
 
 textInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
-    //   display.textContent = input.value;
-    let createList = `
-        <div class="list">
-          <div class="list-checkbox">
-            <label class="custom-checkbox">
-              <input type="checkbox" />
-              <span class="checkmark"></span>
-            </label>
-            <h2 class="text">${textInput.value}</h2>
-          </div>
-          <div class="line"></div>
-        </div>
-      `;
-    listContainer.innerHTML += createList;
+    // Creating the list elements
+    const list = document.createElement("div");
+    list.classList.add("list");
+
+    const listCheckBox = document.createElement("div");
+    listCheckBox.classList.add("list-checkbox");
+
+    const customCheckBox = document.createElement("label");
+    customCheckBox.classList.add("custom-checkbox");
+
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    const checkMark = document.createElement("span");
+    checkMark.classList.add("checkmark");
+
+    const text = document.createElement("h2");
+    text.classList.add("text");
+    text.textContent = `${textInput.value}`;
+
+    const line = document.createElement("div");
+    line.classList.add("line");
+
+    customCheckBox.appendChild(checkBox);
+    customCheckBox.appendChild(checkMark);
+    listCheckBox.appendChild(customCheckBox);
+    listCheckBox.append(text);
+    list.appendChild(listCheckBox);
+    list.appendChild(line);
+
+    // adding the list element to it container when keydown event fires
+    listContainer.appendChild(list);
+
+    //clear the input field when event fires
     textInput.value = "";
-    // console.log(textInput.value);
   }
 });
